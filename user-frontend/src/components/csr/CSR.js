ee02b0481b8f94278a6838b01276f073ea85650c
stasadj <anastasijadjuric@uns.ac.rs>
2022-04-17 17:36:29 +0200

    crs image size

--- user-frontend/src/components/csr/CSR.js
+++ user-frontend/src/components/csr/CSR.js
@@ -9,10 +9,13 @@ const CSR = ({ csr, onGenerate }) => {
   return (
     <Card as={Col} md="2" className="mb-2">
       <Card.Body>
-        <Card.Title>{csr.id}</Card.Title>
-        <Card.Img src="https://cdn-icons-png.flaticon.com/512/2807/2807633.png" />
+        <Card.Title style={{ fontSize: '70%' }}>{csr.id}</Card.Title>
+        <Card.Img
+          src="https://cdn-icons-png.flaticon.com/512/2807/2807633.png"
+          style={{ width: '60%' }}
+        />
       </Card.Body>
-      <ListGroup className="list-group-flush">
+      <ListGroup className="list-group-flush" style={{ fontSize: '70%' }}>
         <ListGroupItem style={{ textAlign: 'left' }}>
           <b>Requested by: </b> {csr.firstName} {csr.lastName}
         </ListGroupItem>

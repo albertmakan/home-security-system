50ac2a68b40bbf630605cbb5194063276c2b9e05
albertmakan <makanalbert@gmail.com>
2022-05-23 16:07:44 +0200

    generate username and password on server

--- admin/src/main/java/com/backend/admin/dto/auth/UserRequest.java
+++ admin/src/main/java/com/backend/admin/dto/auth/UserRequest.java
@@ -5,17 +5,17 @@ import lombok.Data;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
+//import javax.validation.constraints.Pattern;
 import java.util.List;
 
 @Data
 public class UserRequest {
-	@NotBlank
-	private String username;
-	@Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}",
-	message = "Password must contain at least 8 chars, " +
-			"1 uppercase char, 1 lowercase char, a number, and a special char")
-	private String password;
+//	@NotBlank
+//	private String username;
+//	@Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}",
+//	message = "Password must contain at least 8 chars, " +
+//			"1 uppercase char, 1 lowercase char, a number, and a special char")
+//	private String password;
 	@NotBlank
 	private String firstName;
 	@NotBlank

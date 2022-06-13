1e738b9739200cdd3a9a506ccc835f9844112506
albertmakan <makanalbert@gmail.com>
2022-06-13 18:57:39 +0200

    started households page

--- user-frontend/src/components/Home/Home.js
+++ user-frontend/src/components/Home/Home.js
@@ -6,6 +6,7 @@ const Home = () => {
       <img
         src="https://www.svgrepo.com/show/108028/security-system.svg"
         style={{ width: '20%', marginTop: '100px' }}
+        alt="logo"
       />
       <h1>Home Security System</h1>
     </div>

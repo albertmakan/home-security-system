ff1ced8e3ea2706a606c4a9401add5c4e0054b27
Dina Petrov <dina.petrov@gmail.com>
2022-06-22 19:21:38 +0200

    margins and padding classes added to search bar elements

--- user-frontend/src/components/search-filter-bar/SearchFilterBar.js
+++ user-frontend/src/components/search-filter-bar/SearchFilterBar.js
@@ -3,17 +3,17 @@ import React from "react";
 const SearchFilterBar = ({ searchText, filterText, filterValues }) => {
 
     return (
-        <div className="d-flex bd-highlight example-parent">
-            <input className="p-2 flex-grow-1 bd-highlight col-example" placeholder={searchText}></input>
-            <div className="p-2 bd-highlight col-example">{filterText}:</div>
-            <select className="p-2 bd-highlight col-example">
+        <div className="d-flex">
+            <input className="p-2 m-2 flex-grow-1" placeholder={searchText}></input>
+            <div className="p-2 m-2">{filterText}:</div>
+            <select className="p-2 m-2">
                 <option value={"NO_VALUE"}> </option>
                 {filterValues.map((filter, index) => (
                     <option value={filter} key={filter}>{filter}</option>
                 ))}
 
             </select>
-            <button className="p-2 bd-highlight col-example">Search</button>
+            <button className="p-2 m-2">Search</button>
         </div>
     );
 

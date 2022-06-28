083a984455a964090deed1cf51890f68473336eb
Dina Petrov <dina.petrov@gmail.com>
2022-06-28 11:25:27 +0200

    Kie added to admin app, devtools removed, Logs inserted into rulesSession, error log rule

--- admin/src/main/java/com/backend/admin/service/CustomLogger.java
+++ admin/src/main/java/com/backend/admin/service/CustomLogger.java
@@ -1,29 +1,51 @@
 package com.backend.admin.service;
 
-import com.backend.admin.model.Log;
-import com.backend.admin.repository.LogsRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.backend.admin.model.Log;
+import com.backend.admin.repository.LogsRepository;
+
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class CustomLogger {
-    @Autowired
+
+    @Qualifier("rulesSession")
+    private final KieSession rulesSession;
+    
     private LogsRepository logsRepository;
 
     public String info(String message) {
-        logsRepository.save(new Log("INFO", message));
+        Log log = new Log("INFO", message);
+        rulesSession.getAgenda().getAgendaGroup("logs").setFocus();
+        rulesSession.insert(log);
+        rulesSession.fireAllRules();
+        logsRepository.save(log);
         return message;
     }
 
+    
     public String warn(String message) {
-        logsRepository.save(new Log("WARN", message));
-        return message;
+        Log log = new Log("WARN", message);
+        rulesSession.getAgenda().getAgendaGroup("logs").setFocus();
+        rulesSession.insert(log);
+        rulesSession.fireAllRules();
+        logsRepository.save(log);
+        return message; 
+
     }
 
     public String error(String message) {
-        logsRepository.save(new Log("ERROR", message));
+        Log log = new Log("ERROR", message);
+        rulesSession.getAgenda().getAgendaGroup("logs").setFocus();
+        rulesSession.insert(log);
+        rulesSession.fireAllRules();
+        logsRepository.save(log);
         return message;
     }
 

abea29f41e22711b81070badc1e183839fff027b
stasadj <anastasijadjuric@uns.ac.rs>
2022-06-29 03:48:41 +0200

    verification

--- devices/device2.py
+++ devices/device2.py
@@ -1,33 +1,35 @@
 import os
 from time import sleep
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import padding
+from keygen import sign
+from Cryptodome.PublicKey import RSA
+from base64 import b64decode, b64encode
 
 
 folder_path = "../my-house/target/classes/devices"
 device_path = "/house5/door1"
 period = 3  # in seconds
-private_key_str = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDhudzGarRNFoCQ348nPgfsUeNI9mPoZTe3u9XuQgA0y2tRv8bpMwWXoIkZkk3yOSOVxEsYkm0AgUsBGX4Uxj5qY0XVT5ljN8RPis9OpEBbd8HBUNr2+1gM3R+zZ0+4foETSDuMGRhF86QBviqWMvPlb7I1z8CbeJ3XdAz2RJBKBQ9sawEHm5tgPMu85elTU9csSDwJNPdII75kfXeFUCKRd+U2l8Dxab0MeUoapJYMfe2JdJL7hHy6q4LmeLjozTrJAf68cIPrFb0u4MhJ3i5ceLpgqeK5pNIz+gbHnQkezO8rxdoBhZf01nG6DQ2EjzvgIvq3X1aEGuMZyWz/wiWnAgMBAAECggEAA0voRSJkOKV12dbxjvJd98gIejB2/PRXJsKq0K4djN26LpypzeXzb4kwLvcNVpNChgt1/lo97jr1KAoPUmyOv6Mbnlh24gEq2+IanDeQf/67r7XaAQA6K6ZYHemDO27nNhc/R82AwYKqs+zdoJA8kJBVFhlBn6HzeTkKSVC/wn42jXsDG88Rn44JusqEeXbUBwggjO9j/qp4vX8DKg/MISMDP0vI4yfe9j7batt9aRTW0nCadm0bqUcfWjmYUH0sHD3JJ3EWvAKqw0JkHQ5OxwOWqtyvOzvJ4MXaDS2yI/wyT9Z4iGxxG3Mbqv/8VFFFPTxOs9tBUNSWbKJSr7AeaQKBgQD9RoxW2MZXSjbwCAAiWcWMwWXJDXPoixh5ohjM7PJ9YlH6HKbbZ8comr3HF/f7Xj5QNRh30Xwc38a7ShK7jplqFkoxSSm/iHg0ZdjbuOVEx6nFKsYxxKx9WMMLUdw3foTHNZNfCKeAeIttCLh6+gL5jN+qqVYzBvzo202qGTYykwKBgQDkJ3NDt9vLg5gwQoH+ug44+H3LO6AmImBF0uNM2N2Aq6sLouvD/iKRy3DScR+LyeaAXigWaYKy7AN0fRqhDTkTJWSq3w858sZLC/wcolgM/6+NNjtQIBkomek1aLqKFb03QTt2Er+yd0tTIco2+/qlojRO+7ttQqL/ly7lkhPJHQKBgH7MVPIdsJ5+zyY4Oyj3XIltQTH4hDDFgklSt6vQbE4NbREUSwzI1dIkNiJ4g3Pa6MNq8Yb633HtLicRnHM9ntyXsCkggcAb6GIDIyBNXqqicwH8uiCsX9aZG3yNTPyTYJIC5nbLdGBC+TprBUC77cVVm5xrDnaZZwCE2krwOEv1AoGBALObetnse1MTHVOgh8WHonqm5CCBp+ldVnyhL97nOjh47AYVE5UPExfDF+YSt8rL/nE/rj82gHVp5q5lbfkq96ti3ITHZK0xuXiKzsWb58UT45c/AiJpomScBULOV0stIe/FopGo4NAVqW6gQxhV9VM/Bf7HnRy+QKME2R5QiNLhAoGBANLFGcKf1SgRZhMLD7xDSe1Ppi6kcxmaBKiNnd97Inb9kwCMOrKN0KDucaVIAWcn8WaNCXmjclFoxVd0G9cGGvSY5NdomIkmxUuQm2wY1fP4YHvkpj5lcdqq4lLFNWoy8rbm/9J9dr04z7PRLHU6XDt+wobI4hpkrBwYXy4pEgxe"
-private_key = serialization.load_pem_private_key(
-            str.encode(private_key_str),
-            password=None,
-            backend=default_backend()
-        )
-encrypted = private_key.encrypt(
-        "door message 1",
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
+private_key_str = "\
+MIICXAIBAAKBgQCvoRpDUw84yGIRHRhGV9TkFSBEBZBCeyyGCtTi70Vbmcan7r8l\
+GipBmPurE9/CbyjuZPGO5xICJRKHhUeBRptEnlVI9KcACzszwS3WAHLE6j8w+I56\
+1Lw2uTrY3ET+pmvkA7NEf+LwAadvhlEnDQfwwXOlu9kuovxA9vRMHS33uwIDAQAB\
+AoGAEer85nHNY5d8pc6oQNj0ynY7Tfqb/qRZZQLVjaIn8HyjpKi6DeYkTyqf671s\
+foY1EKlHbwhfsSH+Hh8OA1WO696yRhSZr5pTfyN1VlWHGY+NpK07dxwbQs76ujON\
+X+qZeLN0YwM378Zemz3QJDT5k08QadK5nWZBBe8Bq4p6SuUCQQC3mFaCgOfZTnyi\
+3ZTrFnyIOcOYapDIfO0IsKOjfg2RIIfo4cw3TLY22hAdPGBdbVlJB+RAEiGKAOo6\
+eQZgGkwFAkEA9OSLnzQeiaA8glklHoUtZZFaz4VjiG7VAQugkHQoaq5Q/2g0YdIQ\
+U8t+CZ7xqon+6A9sFUux+IQ6+5Hfj7ZAvwJAeVQVJ51kmrlBqKfieBGeLrBt0FXs\
+ZHIaezmwiqUM9jGXX5GI0arCaDFnoMp7rIJNFPRAJQ9WVssP/6BnVC9MuQJBANB1\
+t7BmolQO1VMz5RgMz670w3OBHU/m4GSKbDxq1HizN/mhz20xbnqV7wzrgtXjr9wE\
+PMIH9FAS5058RuzDfGsCQAwp+0BqFkUULpUIscpG7P0Mf1qExBkO3bCMfih4h+UB\
+9ljlczyS/4HHI6ohmlqVtMgY9Y8cJLrECt0RS5ySHiw="
+
 path = folder_path+"/".join(device_path.split('/')[:-1])
 if not os.path.exists(path):
     os.makedirs(path)
 
-for i in range(50):
+key = RSA.importKey(b64decode(private_key_str))
+
+for i in range(2):
     with open(folder_path+device_path, 'w') as f:
-        f.write(encrypted)
+        f.write("Message " + sign(key, "Message"))
     sleep(period)
